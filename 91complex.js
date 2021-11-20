ba9fea0ea8dd68dca9eb373fa48d0d452c209b34
stacy <ada.davis27@gmail.com>
2021-11-21 02:04:49 +0300

    new file

---
+++ 91complex.js
@@ -0,0 +1,24 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+myMusic.push({
+  "artist": "Falling in Reverse",
+    "title": "Coming Home",
+    "release_year": 2017,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": false
+});
