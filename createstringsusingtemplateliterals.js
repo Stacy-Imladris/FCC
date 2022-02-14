2a8aa2c30095bc767b06276d9449f82bc4351d27
stacy <ada.davis27@gmail.com>
2022-02-14 17:10:26 +0300

    new file

---
+++ createstringsusingtemplateliterals.js
@@ -0,0 +1,11 @@
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  const failureItems = result.failure.map(m => `<li class="text-warning">${m}</li>`)
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
