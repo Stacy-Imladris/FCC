005df33cb5d90ff753d1d6f91e797f0a7d06a050
stacy <ada.davis27@gmail.com>
2022-06-16 00:40:54 +0300

    new file

---
+++ profilelookup.js
@@ -0,0 +1,35 @@
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  let person = contacts.find(f => f.firstName === name)
+  if (!person) return 'No such contact'
+  else if (person.hasOwnProperty(prop)) return person[prop]
+  else return 'No such property'
+}
+
+lookUpProfile("Akira", "likes");
